04fbd7e4ac29c23cfd066047164f93b43c407089
Wade Rogers <83975976+WdRgrs@users.noreply.github.com>
2021-06-29 15:49:12 -0500

    home.html error

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ rollbar.log("This is the traceability rollbar.log");
 
 
 app.get('/', function(req, res) {
+    rollbar.log('Hello World');
+    rollbar.error('User tried to access a broken link');
+    res.sendFile(path.join(__dirname, 'public/home.html'))
     res.sendFile(path.join(__dirname, '/public/index.html')) //res is a built in body, sendfile being a build in method to send back a file at a specific path
 //path.join - join the location of index.html to the current directory
 }); //__dirname - always the first argument of the directory at this location (monitoring-interactive)
